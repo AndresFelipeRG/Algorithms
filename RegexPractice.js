afdcea6b5d08a0e7e4cd285159a041fb759f1107
Andres Felipe Rincon Gamboa <andres.rincon@mail.mcgill.ca>
2019-09-08 15:59:54 -0400

    Create RegexPractice.js

---
+++ RegexPractice.js
@@ -0,0 +1,3 @@
+str = "ashdhdadda ajdad-b244 http://hello.world.com/dadjad-adna12313?&?%#https://helo133.ddaf.affa.world.com 12131441589hj$%#@#";
+regex = /(?:http|https){1}(:\/\/)([0-9a-z-]+){1}(\.([0-9a-z-]+)){1,}(\/{0,1})/g;
+console.log([...str.matchAll(regex)].map((arr)=>{return arr[0].replace(/http:\/\/|https:\/\//g, '');}));
